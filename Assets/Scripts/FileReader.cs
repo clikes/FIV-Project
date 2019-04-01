f69786c692b505e89cfba0e79d0a367ffcc228c4
Like <chenlike695@outlook.com>
2019-04-01 13:27:15 +0800

    add system.data plugin

---
+++ Assets/Scripts/FileReader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+public class FileReader {
+    public static DataTable ReadFile(string filename)
+    {
+
+
+
+        return null;
+    }
+}
