ff31d9c50be5e0627b91a8ff6ccd2e0a1e56726e
Like <chenlike695@outlook.com>
2019-04-03 23:37:30 +0800

    dynamic change dropdown menu

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -3,22 +3,44 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEditor;
 using UnityEngine;
-
-
+using DataIO;
+using UnityEngine.UI;
+using static UnityEngine.UI.Dropdown;
 
 public class UIController : MonoBehaviour {
 
-	void Start () {
+    public Dropdown[] Selectdps;
 
+    public DataExtractor temp;
+
+    void Start () {
+        foreach (var dp in Selectdps)
+        {
+            dp.ClearOptions();
+        }
     }
 
     public void ClickImportData()
     {
-        string filePath = EditorUtility.OpenFilePanel("Find File", Application.dataPath, "*.*");
+        string filePath = EditorUtility.OpenFilePanel("Import File", "", "csv");
+        FileReader.ReadFile(filePath);
+        temp = FileReader.Datas[FileReader.Datas.Count-1];
+        UpdateSelectPanel();
+    }
 
-
-
-
+    void UpdateSelectPanel()
+    {
+        List<OptionData> options = new List<OptionData>();
+        options.Add(new OptionData("-"));
+        foreach (var item in temp.columnName)
+        {
+            options.Add(new OptionData(item));
+        }
+        foreach (var item in Selectdps)
+        {
+            item.AddOptions(options);
+        }
+        //Selectdp1.AddOptions(options);
     }
 
 
