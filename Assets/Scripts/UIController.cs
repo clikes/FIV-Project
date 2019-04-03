4f251ae11fe9c3fefdb944bed4dff4be73641d2c
zy18305 <zy18305@nottingham.edu.cn>
2019-04-03 21:23:46 +0800

    新增弹窗选择文件

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -7,8 +7,7 @@ using UnityEngine;
 
 
 public class UIController : MonoBehaviour {
-    //List<int> test;
-	// Use this for initialization
+
 	void Start () {
 
     }
@@ -22,28 +21,7 @@ public class UIController : MonoBehaviour {
 
     }
 
-    private void OnGUI()
-    {
-        if (GUI.Button(new Rect(10, 10, 100, 50), "Open"))
-        {
-            OpenFileName openFileName = new OpenFileName();
-            openFileName.structSize = Marshal.SizeOf(openFileName);
-            openFileName.filter = "Excel文件(*.xlsx)\0*.xlsx";
-            openFileName.file = new string(new char[256]);
-            openFileName.maxFile = openFileName.file.Length;
-            openFileName.fileTitle = new string(new char[64]);
-            openFileName.maxFileTitle = openFileName.fileTitle.Length;
-            openFileName.initialDir = Application.streamingAssetsPath.Replace('/', '\\');//默认路径
-            openFileName.title = "窗口标题";
-            openFileName.flags = 0x00080000 | 0x00001000 | 0x00000800 | 0x00000008;
 
-            if (LocalDialog.GetSaveFileName(openFileName))
-            {
-                Debug.Log(openFileName.file);
-                Debug.Log(openFileName.fileTitle);
-            }
-        }
-    }
     // Update is called once per frame
     void Update () {
 		
