53f40a8cf3a9b455d63395ec06d404e04b024637
Like <chenlike695@outlook.com>
2019-04-03 23:01:21 +0800

    update DataIO namespace
    namespace for read file and write file

---
+++ Assets/Scripts/DataIO/DataExtractor.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+namespace DataIO
+{
+    public class DataExtractor
+    {
+
+        public List<string> columnName;
+
+        public List<Type> types;
+
+        private DataTable dataTable;
+
+
+        public DataExtractor(DataTable dataTable)
+        {
+            this.dataTable = dataTable;
+            columnName = new List<string>();
+
+            types = new List<Type>();
+
+            foreach (DataColumn col in dataTable.Columns)
+            {
+                columnName.Add(col.ColumnName);
+                types.Add(col.DataType);
+            }
+        }
+
+        public List<int> GetIntColumn(string columnName)
+        {
+            List<object> col = new List<object>();
+
+            foreach (DataColumn column in dataTable.Columns)
+            {
+                if (column.ColumnName == columnName)
+                {
+                    if (column.DataType != typeof(int))
+                    {
+                        throw new FormatException("The column " + columnName + " is not match type Int");
+                    }
+                }
+            }
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                col.Add(dr[columnName]);
+            }
+
+            List<int> colConverted = col.ConvertAll(item => (int)item);
+
+            return colConverted;
+        }
+
+        public List<float> GetFloatColumn(string columnName)
+        {
+            List<object> col = new List<object>();
+
+            foreach (DataColumn column in dataTable.Columns)
+            {
+                if (column.ColumnName == columnName)
+                {
+                    if (column.DataType != typeof(float))
+                    {
+                        throw new FormatException("The column " + columnName + " is not match type float");
+                    }
+                }
+            }
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                col.Add(dr[columnName]);
+            }
+
+            List<float> colConverted = col.ConvertAll(item => (float)item);
+
+            return colConverted;
+        }
+
+        public List<string> GetStringColumn(string columnName)
+        {
+            List<object> col = new List<object>();
+
+            foreach (DataColumn column in dataTable.Columns)
+            {
+                if (column.ColumnName == columnName)
+                {
+                    if (column.DataType != typeof(string))
+                    {
+                        throw new FormatException("The column " + columnName + " is not match type string");
+                    }
+                }
+            }
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                col.Add(dr[columnName]);
+            }
+
+            List<string> colConverted = col.ConvertAll(item => (string)item);
+
+            return colConverted;
+        }
+
+    }
+}
