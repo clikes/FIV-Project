602c5847aca88b43f2ae12cfba230f29fbda5212
Like <chenlike695@outlook.com>
2019-04-03 20:58:51 +0800

    update axis add a cone to the edge

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UIController : MonoBehaviour {
-
+    //List<int> test;
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+    }
 
     public void ClickImportData()
     {
