04025825534e96f6aefe8f11252400cc9cc11bf2
zy18305 <zy18305@nottingham.edu.cn>
2019-04-03 21:00:24 +0800

    新增windows选择文件弹窗

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
+
+
 
 public class UIController : MonoBehaviour {
 
@@ -11,9 +14,35 @@ public class UIController : MonoBehaviour {
 
     public void ClickImportData()
     {
-        Debug.Log("test");
+       
+           
+        
+
+ 
     }
 
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 100, 50), "Open"))
+        {
+            OpenFileName openFileName = new OpenFileName();
+            openFileName.structSize = Marshal.SizeOf(openFileName);
+            openFileName.filter = "Excel文件(*.xlsx)\0*.xlsx";
+            openFileName.file = new string(new char[256]);
+            openFileName.maxFile = openFileName.file.Length;
+            openFileName.fileTitle = new string(new char[64]);
+            openFileName.maxFileTitle = openFileName.fileTitle.Length;
+            openFileName.initialDir = Application.streamingAssetsPath.Replace('/', '\\');//默认路径
+            openFileName.title = "窗口标题";
+            openFileName.flags = 0x00080000 | 0x00001000 | 0x00000800 | 0x00000008;
+
+            if (LocalDialog.GetSaveFileName(openFileName))
+            {
+                Debug.Log(openFileName.file);
+                Debug.Log(openFileName.fileTitle);
+            }
+        }
+    }
     // Update is called once per frame
     void Update () {
 		
